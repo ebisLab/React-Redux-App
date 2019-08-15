882ecac29b2720f46cf2ef7d6c61af16484f9e8a
ebisLab <ebaiden@techie.com>
2019-08-15 19:09:24 -0400

    added Loader inside button

--- api-app/src/components/WebcamList.js
+++ api-app/src/components/WebcamList.js
@@ -1,13 +1,26 @@
 import React from 'react';
-import {connect} from 'react-redux'
+import {connect} from 'react-redux';
+import Loader from 'react-loader-spinner'
 
 const WebCamList = (props) => {
-    return <h1>{props.test}</h1>
+    return  (<>
+    <h1>Welcome to Studio Ghibili</h1>
+    <button>
+        {props.isLoading ?
+        <Loader
+        type="Puff"
+        color="#00BFFF"
+        height="100"
+        width="100" /> : 'Get info'
+        }
+        </button>
+    </>)
+
 }
 
 const mapStateToProps = state =>{
     return {
-        test: state.test
+        isLoading: state.isLoading
     };
   };
 
