d1699496a9cf17f74271cd9c2521685225acbfe7
ebisLab <ebaiden@techie.com>
2019-08-15 18:20:16 -0400

    reducer set up

---
+++ api-app/src/reducers/index.js
@@ -0,0 +1,10 @@
+const initialState = {
+    webcams: []
+}
+
+export const reducer = (state = initialState, action) => {
+    switch(action.type){
+        default:
+            return state;
+    }
+}
