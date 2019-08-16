8bab10b9d0daa5a0af604983c00e90b421943f63
ebisLab <ebaiden@techie.com>
2019-08-16 07:31:08 -0400

    axios call

--- api-app/src/actions/webCamActions.js
+++ api-app/src/actions/webCamActions.js
@@ -1,9 +1,14 @@
+import axios from 'axios'
+
 export const FETCHING_WEBCAM_DATA = 'FETCHING_WEBCAM_DATA';
 
 export const getData = () => {
     return dispatch => {
         //dispatch an action that tells reducer to transition to loading space
-        dispatch({type: 'FETCHING_WEBCAM_DATA'})
+        dispatch({type: 'FETCHING_WEBCAM_DATA'});
+        axios.get(`https://ghibliapi.herokuapp.com/films`)
+        .then(res => console.log(res))
+        .catch(err => console.log(err))
 
     }
 }
